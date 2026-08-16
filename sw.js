06b4d4a470bdb8eacdf43140b29af46bfc653a41
StormDog2 <floyt1331@gmail.com>
2026-08-16 17:41:22 +0200

    Add files via upload
    V2

---
+++ sw.js
@@ -0,0 +1,50 @@
+/* Build your monster - offline storage
+   Bump VERSION whenever index.html changes, so iPads pick up the new copy. */
+
+var VERSION = "v1";
+var CACHE   = "monster-" + VERSION;
+
+var ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./apple-touch-icon.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", function(e){
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(function(c){ return c.addAll(ASSETS); })
+      .then(function(){ return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){
+        if (k !== CACHE) return caches.delete(k);
+      }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function(e){
+  if (e.request.method !== "GET") return;
+
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      var live = fetch(e.request).then(function(res){
+        if (res && res.status === 200 && res.type === "basic"){
+          var copy = res.clone();
+          caches.open(CACHE).then(function(c){ c.put(e.request, copy); });
+        }
+        return res;
+      }).catch(function(){ return hit; });
+
+      return hit || live;
+    })
+  );
+});
